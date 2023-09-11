b92319096ae4bd4873b4a58708657353c79b2641
lnee94 <73306958+lnee94@users.noreply.github.com>
2023-09-11 12:06:17 -0400

    Add files via upload

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://github.com/lnee94/holdup/issues
+Bug-Submit: https://github.com/lnee94/holdup/issues/new
+Changelog: https://github.com/lnee94/holdup/blob/master/CHANGES
+Documentation: https://github.com/lnee94/holdup/wiki
+Repository-Browse: https://github.com/lnee94/holdup
+Repository: https://github.com/lnee94/holdup.git
